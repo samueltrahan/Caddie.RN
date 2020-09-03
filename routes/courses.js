b2c5ed0c332c242f0b550e8c843fb55a16c685c6
Samuel Trahan <samtrahan91@gmail.com>
2020-09-03 11:08:30 -0500

    courses routes

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const courseCtrl = require("../controllers/courses");
+const coursesCtrl = require("../controllers/courses");
+
+
+router.get('/search', isLoggedIn, coursesCtrl.search);
+router.get('/api', isLoggedIn, coursesCtrl.courseQuery);
+router.post('/save', isLoggedIn, coursesCtrl.create);
+router.get('/courselist', isLoggedIn, coursesCtrl.index);
+router.post('/:id/details', isLoggedIn, coursesCtrl.courseDetails);
+router.delete('/:id', isLoggedIn, coursesCtrl.deleteCourse);
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
