ede54eb99cc3d0209d37977799ff13b166009254
Samuel Trahan <samtrahan91@gmail.com>
2020-09-02 20:07:06 -0500

    database folde set up

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+db.on("connected", function () {
+  console.log(`Mongoose connected to ${db.host}`);
+});
+
+module.exports = mongoose;
