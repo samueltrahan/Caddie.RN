15b7160a2557f961d09b93c0f0f2429bb263330a
Samuel Trahan <samtrahan91@gmail.com>
2020-09-08 16:51:31 -0500

    json web token back in the project

--- server.js
+++ server.js
@@ -1,44 +1,29 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const session = require('express-session');
-const passport = require('passport');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const favicon = require("serve-favicon");
+const passport = require("passport");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
 
-require('dotenv').config();
-require('./config/database');
+require("dotenv").config();
+require("./controllers/users");
+require("./config/database");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/user');
-const coursesRouter = require('./routes/courses');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/user");
+const coursesRouter = require("./routes/courses");
 
 const app = express();
 
-
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}))
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitalized: true
-}))
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(function(req, res, next) {
-  res.locals.user = req.user;
-  next()
+app.listen(port, ()=> {
+  console.log(`Express is listening on port ${port}`)
 })
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/courses', coursesRouter);
-
-app.use(function(req, res) {
-  res.status(404).send('/error')
-});
 
 module.exports = app;
