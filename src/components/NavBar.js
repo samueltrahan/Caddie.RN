9ad997ca78c015479bc9fdaaea895a64abc052d3
Samuel Trahan <samtrahan91@gmail.com>
2020-09-11 11:24:39 -0500

    nav bar ccomponent

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const NavBar = () => {
+  let nav = 
+  <>
+    <nav>
+      <div className="nav-wrapper">
+        <ul id="nav-mobile" className="right">
+
+        </ul>
+
+      </div>
+    </nav>
+  </>
+
+  return (
+    <>
+    {nav}
+    </>
+  )
+}
+
+export default NavBar;
