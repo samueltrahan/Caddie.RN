20071d37c7411dcdcbe5416c5d27585223e9a6fd
Samuel Trahan <samtrahan91@gmail.com>
2020-09-21 10:32:47 -0500

    api call set up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react'
 import SearchBar from './components/SearchBar'
 import NavBar from './components/NavBar'
 
+
+const key = process.env.REACT_APP_API
+
 export default function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [cities, setCities] = useState([]);
@@ -11,10 +14,14 @@ export default function App() {
     setSearchTerm({input})
   }
 
+  const getCity = async () => {
+    const response = await fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=golf+courses+${searchTerm}&key=${key}`)
+  }
+
   const handleSearchSubmit = async (event) => {
     event.preventDefault();
-    // const city = await getCity(searchTerm);
-    // console.log(city)
+    const city = await getCity(searchTerm);
+    console.log(city)
   }
 
 
