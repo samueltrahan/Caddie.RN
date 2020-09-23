5b524740f26d5e7d259c248f7b9fe3b0a6d49e09
Samuel Trahan <samtrahan91@gmail.com>
2020-09-23 10:42:36 -0500

    backend file set up for api request

--- src/App.js
+++ src/App.js
@@ -4,24 +4,17 @@ import NavBar from './components/NavBar'
 import axios from 'axios'
 
 
-const key = process.env.REACT_APP_API_KEY
-
 export default function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [courses, setCourses] = useState([]);
 
 
 
-  const getCity = async () => {
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=golf+courses+${searchTerm}&key=${key}`)
-    const data = await response.json()
-    console.log(data)
-  }
+
 
   const handleSearchSubmit = async (event) => {
     event.preventDefault();
-    const city = await getCity();
-    console.log(city)
+    // const city = await getCity();
   }
 
 
