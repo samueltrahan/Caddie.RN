0c277100a6c3246a7764922f6695e9c5b419230b
Samuel Trahan <samtrahan91@gmail.com>
2020-09-24 09:59:48 -0500

    courses displaying on the page

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -3,7 +3,12 @@ import React from 'react';
 const CourseList = ({course}) => {
   return (
     <>
-    
+    <div>
+      <ul>
+      <li>{course.name}</li>
+      <li>{course.formatted_address}</li>
+      </ul>
+    </div>
     </>
   )
 }
