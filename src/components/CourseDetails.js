0eb72b352e5c2889534948b45924c40b1caca71f
Samuel Trahan <samtrahan91@gmail.com>
2020-09-28 13:04:47 -0500

    course rating added to course detail page

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -1,28 +1,28 @@
-import React, {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const CourseDetails = () => {
-  const [courseDetails, setCourseDetails] = useState({})
-  const {id} = useParams();
+  const [courseDetails, setCourseDetails] = useState({});
+  const { id } = useParams();
 
   useEffect(() => {
     const getCourseDetails = async (courseId) => {
-      axios.get('/details', {
-        params: {
-          courseId: courseId,
-        },
-      })
-      .then((response) => {
-        setCourseDetails(response.data.result)
-      })
-      .catch((err) => console.log(err));
-    }
+      axios
+        .get("/details", {
+          params: {
+            courseId: courseId,
+          },
+        })
+        .then((response) => {
+          setCourseDetails(response.data.result);
+        })
+        .catch((err) => console.log(err));
+    };
     getCourseDetails(id);
-  }, [id])
- 
+  }, [id]);
+
   console.log(courseDetails);
-  
 
   // const reviews = courseDetails.reviews.map((review) => {
   //   return review
@@ -30,13 +30,16 @@ const CourseDetails = () => {
 
   return (
     <>
-    <div>
-     <h1><a href={courseDetails.website}>{courseDetails.name}</a></h1>
-     <h2>{courseDetails.formatted_address}</h2>
-     <h2>{courseDetails.formatted_phone_number}</h2>
-    </div>
+      <div>
+        <h1>
+          <a href={courseDetails.website}>{courseDetails.name}</a>
+        </h1>
+        <h2>{courseDetails.formatted_address}</h2>
+        <h2>{courseDetails.formatted_phone_number}</h2>
+        <h3>Course Rating: {courseDetails.rating}</h3>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default CourseDetails;
