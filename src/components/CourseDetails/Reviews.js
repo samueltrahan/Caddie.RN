e8c0fdd72abf7a99f85c5acc9a77bd9f77334206
Samuel Trahan <samtrahan91@gmail.com>
2020-09-29 13:16:33 -0500

    review styling

--- src/components/CourseDetails/Reviews.js
+++ src/components/CourseDetails/Reviews.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React from "react";
+import "./Reviews.css";
 
-const Reviews = ({reviews}) => {
-return (
-  <>
-  <h1>Reviews</h1>
-  {reviews.map((review) => 
-  <div>
-    <h2>{review.author_name}</h2>
-    <h4>{review.text}</h4>
-  <h5>{review.relative_time_description}</h5>
-  </div>
-  )}
-  </>
-)
-}
+const Reviews = ({ reviews }) => {
+  return (
+    <>
+      <h1 className="review-header">Reviews</h1>
+      {reviews.map((review) => (
+        <div className="row">
+          <div className="col s12 m6">
+            <div className="card blue-grey darken-1">
+              <div className="card-content white-text">
+                <span className="card-title">{review.author_name}</span>
+                <h4>{review.text}</h4>
+                <h5>{review.relative_time_description}</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
-
-export default Reviews
+export default Reviews;
