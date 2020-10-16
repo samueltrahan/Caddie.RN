540ad51aa52e9017680291736231acccc54cfa16
Samuel Trahan <samtrahan91@gmail.com>
2020-10-16 10:44:14 -0500

    google maps working

--- src/components/GoogleMaps/GoogleMaps.js
+++ src/components/GoogleMaps/GoogleMaps.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {GoogleMap, withScriptjs, withGoogleMap} from 'react-google-maps'
+import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps'
 
 function Map() {
   return (
@@ -20,7 +20,7 @@ export default function GoogleMaps() {
 
   return (
     <div style={{width: '100vw', height: '100vh'}}>
-        <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAP_API}`}
+        <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
         loadingElement={<div style={{height: '100%'}} />}
         containerElement={<div style={{height: '400px'}} />}
         mapElement={<div style={{height: '100%'}} />}
