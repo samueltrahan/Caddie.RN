aa594b67b708ec3a09b57d81d4797b9315239d7d
Samuel Trahan <samtrahan91@gmail.com>
2020-10-16 09:25:32 -0500

    api key implemented

--- src/components/GoogleMaps/GoogleMaps.js
+++ src/components/GoogleMaps/GoogleMaps.js
@@ -12,7 +12,7 @@ const WrappedMap = withScriptjs(withGoogleMap(Map))
 export default function GoogleMaps() {
   return (
     <div style={{width: '100vw', height: '100vh'}}>
-      <WrappedMap googleMapURL={`https://maps.googleapis.come/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+      <WrappedMap googleMapURL={`https://maps.googleapis.come/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
       loadingElement={<div style={{height: "100%"}}/>}
       containerElement={<div style={{height: "100%"}}/>}
       mapElement={<div style={{height: "100%"}}/>}
