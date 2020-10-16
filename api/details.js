ab7926d5a8f649099934f61ab6d640f3ee898e7c
Samuel Trahan <samtrahan91@gmail.com>
2020-10-16 10:25:24 -0500

    google maps displays on page

--- api/details.js
+++ api/details.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const key = process.env.API_KEY;
+const key = process.env.REACT_APP_API_KEY;
 
 function getDetails(req, res) {
   axios.get(
--- src/components/GoogleMaps/GoogleMaps.js
+++ src/components/GoogleMaps/GoogleMaps.js
@@ -1,31 +1,31 @@
 import React from 'react'
-import {GoogleMap, useLoadScript, Marker, InfoWindow} from '@react-google-maps/api'
+import {GoogleMap, withScriptjs, withGoogleMap} from 'react-google-maps'
 
-const libraries = 'places'
-const mapContainerStyle = {
-  width: '100vw',
-  height: '100vh'
+function Map() {
+  return (
+    <GoogleMap 
+    defaultZoom={10}
+    defaultCenter={{
+      lat: 30.2672,
+      lng: -97.7431
+    }}
+    />
+  )
 }
-const center = {
-  lat: 30.2672,
-  lng: -97.7431
-}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
 
 export default function GoogleMaps() {
-  const {isLoaded, loadError} = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_API_KEY,
-    libraries,
-  });
-
-  if(loadError) return 'Error loading maps'
-  if(isLoaded) return 'Loading Maps';
-
-
-
 
   return (
-    <div>
-        <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center} ></GoogleMap>
+    <div style={{width: '100vw', height: '100vh'}}>
+        <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAP_API}`}
+        loadingElement={<div style={{height: '100%'}} />}
+        containerElement={<div style={{height: '400px'}} />}
+        mapElement={<div style={{height: '100%'}} />}
+        />
     </div>
   )
 }
+
